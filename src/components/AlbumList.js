91d0c9cdc2e1718efa5e5f479a949bdb6a844bb8
aspiringguru <bmatthewtaylor@gmail.com>
2017-11-04 17:53:28 +1000

    adding json file

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,40 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import ApiUtils from '../../ApiUtils';
-//import axios from 'axios';//removed because axios keep crashing the app.
+import ApiUtils from '../../ApiUtils';//should move ApiUtils to src/components
 
 class AlbumList extends Component {
   state = { albums: [] };
-  //componentWillMount is a lifecyle method.
-  //https://reactjs.org/docs/react-component.html
   componentWillMount() {
     console.log('componentWillMount start in AlbumList');
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
-    //.then(ApiUtils.checkStatus)
-    //.then((response) => response.json())
-    //.then((responseData) => { console.log(responseData); });
-    .then(response => this.setState({ albums: response.data }));
-    //setState update component state, rerender.
-    //.then(data => this.setState({ albums: data }));
-    //do not need to import fetch. linter does not like this. why?
-    //alternative method
-    //fetch('https://rallycoding.herokuapp.com/api/music_albums')
-    //.then(response => response.json())
-    //.then(responseData) => {
-    //  console.log(responseData);
-    //}
-
-    //axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-    //  .then(response => console.log(response));
-    console.log('componentWillMount end in AlbumList');
+      .then(ApiUtils.checkStatus)
+      .then(response => this.setState({ albums: response.data }))
+      //.then(response => response.json())
+      .then(responseData => {
+        console.log('responseData:', responseData);//contains loaded data
+      });
   }
 
+//  renderAlbums() {
+//    return this.state.albums.map(album => <Text>{album.title}</Text>);
+//  }
+
   render() {
-    console.log(this.state);
+    console.log('this.state:', this.state);//{albums: Array(0)}
     return (
       <View>
-        <Text>Album List!!!!</Text>
-
+        <Text>blah</Text>
       </View>
     );
   }
