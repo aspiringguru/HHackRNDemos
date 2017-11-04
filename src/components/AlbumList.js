7064edff5adb7f7dec0a1fee3a9be56de6a397d6
aspiringguru <bmatthewtaylor@gmail.com>
2017-11-04 15:54:24 +1000

    fetch with error control added

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import ApiUtils from '../../ApiUtils';
+//import axios from 'axios';//removed because axios keep crashing the app.
 
 class AlbumList extends Component {
+  //componentWillMount is a lifecyle method.
+  //https://reactjs.org/docs/react-component.html
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+    console.log('componentWillMount start in AlbumList');
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(ApiUtils.checkStatus)
+    .then((response) => response.json())
+    .then((responseData) => {
+        console.log(responseData);
+    });
+    //.then(data => this.setState({ albums: data }));
+    //do not need to import fetch. linter does not like this. why?
+    //alternative method
+    //fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    //.then(response => response.json())
+    //.then(responseData) => {
+    //  console.log(responseData);
+    //}
+
+    //axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    //  .then(response => console.log(response));
+    console.log('componentWillMount end in AlbumList');
   }
 
   render() {
     return (
       <View>
         <Text>Album List!!!!</Text>
-        
+
       </View>
     );
   }
